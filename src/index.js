fa5c0673d50bcc5c5c2461a7e46bb7bcd9b61dda
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-08-03 16:45:21 +0100

    Initial commit :sparkles:

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import  Counters from "./components/counters";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Counters />,
   document.getElementById('root')
 );
 
